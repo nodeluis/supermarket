d7a2bdefa844d6468953c26dfc4efa17c3490bdc
luis alfonso <lruiseacm@gmail.com>
2021-03-27 20:00:04 -0400

    android

--- app/src/main/java/com/example/supermercado/ui/dialogs/confirmFragment.java
+++ app/src/main/java/com/example/supermercado/ui/dialogs/confirmFragment.java
@@ -1,16 +1,23 @@
 package com.example.supermercado.ui.dialogs;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.supermercado.R;
 
-public class confirmFragment extends Fragment {
+public class confirmFragment extends Fragment implements View.OnClickListener {
+
+    Button cancel,ok;
+    Context context;
+    DataConfirm listen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,7 @@ public class confirmFragment extends Fragment {
         if (getArguments() != null) {
 
         }
+        context=getActivity();
     }
 
     @Override
@@ -26,8 +34,31 @@ public class confirmFragment extends Fragment {
         // Inflate the layout for this fragment
         View root=inflater.inflate(R.layout.fragment_confirm, container, false);
 
+        cancel=root.findViewById(R.id.fargment_confirm_cancel);
+        ok=root.findViewById(R.id.fargment_confirm_ok);
 
+        cancel.setOnClickListener(this);
+        ok.setOnClickListener(this);
 
         return root;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        listen= (DataConfirm) getTargetFragment();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.fargment_confirm_cancel){
+            listen.senddataConfirm(false);
+        }else if(view.getId()==R.id.fargment_confirm_ok){
+            listen.senddataConfirm(true);
+        }
+    }
+
+    public interface DataConfirm{
+        void senddataConfirm(Boolean data);
     }
 }
